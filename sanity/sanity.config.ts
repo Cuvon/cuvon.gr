04c868a664e9caa07cebc6486d4b5a92bdbf9a22
dmemos <107873118+dmemos@users.noreply.github.com>
2026-05-18 22:46:26 +0300

    Update sanity.config.ts

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -4,7 +4,6 @@
 
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import { visionTool }    from '@sanity/vision'
 import { schemaTypes }   from './schemas'
 
 export default defineConfig({
